4ca69723c7609ea0a420767dcdc0c746cca302f8
Vladislav Romanovich Bauer <bauerxvii@gmail.com>
2023-07-14 16:00:25 +0700

    completed! Last commit

--- script.js
+++ script.js
@@ -8,39 +8,68 @@
     console.log(btn)
 
 
-    const notes =[ 'учиться', 'kushat' ]
-    notes.push ('asd')
-    console.log (notes)
+    const notes =[ {
+       title: 'учиться',
+       completed: true,
+        },
+        {
+        title: 'кушать',
+        completed: false,
+        }
+    ];
 
-    
-    function getNote(title){
+    function getNote(title, index){
         return `<li class="list-group-item d-flex justify-content-between align-items-center" id="liList">
-            <span>${title}</span>
+            <span class=" ${ title.completed ? 'text-decoration-line-through' : ''}">${title.title}</span>
             <span>
-                <span class="btn btn-small btn-success">&check;</span>
-                <span class="btn btn-small btn-danger"> &ltimes;</span>
+                <span class="btn btn-small
+                 btn-${ title.completed ? 'warning' : 'success' }" 
+                 data-index="${index}" data-type="toggle">&check;</span>
+                <span class="btn btn-small btn-danger" data-index="${index}" data-type="remove"> &ltimes;</span>
             </span>
         </li>`
     };
 
     function render (){
+        ul.innerHTML = '';
+        if ( notes == 0 ){
+            ul.innerHTML = '<p>Здесь ничего нет</p>'
+        }
         for ( let i = 0; i < notes.length; i++){
             ul.insertAdjacentHTML ('beforeend', 
-        getNote(notes[i]))
+        getNote(notes[i], i))
         }
     }
     render ();
-
+    
     btn.onclick = function (){
         if ( input.value == 0 ){
             alert ('Введите заметку...')
             return;
         }
-        else{
-            ul.insertAdjacentHTML ('beforeend', getNote (input.value))
-        }
+            const newNote = {
+                    title: input.value,
+                    completed: true,
+            }
+                notes.push (newNote),
+                render(newNote);
         input.value = ' ';
+        console.log (notes)
+    };
+
+    ul.onclick = function (event){
+        console.log (event.target.dataset.index);
+        if (event.target.dataset.index){
+            const index = parseInt(event.target.dataset.index)
+            const type = event.target.dataset.type
+                if ( type == 'toggle'){
+                    notes[index].completed = !notes[index].completed
+                }
+                else if ( type == 'remove'){
+                    notes.splice(index, 1)
+                }
+        }
+         render();
     }
-
     
-
+    
